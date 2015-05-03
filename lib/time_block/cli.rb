796fb5e02bff15d874062ca5ff4d0caf32de5b53
Faizal Zakaria <phaibusiness@gmail.com>
2015-05-03 18:29:36 +0800

    Add terminal-notifier gem as runtime dependancy

--- lib/time_block/cli.rb
+++ lib/time_block/cli.rb
@@ -2,11 +2,17 @@ require 'time_block'
 
 class TimeBlock::CLI
   def self.start(*args)
+    fail 'No terminal-notifier, please install it, brew install terminal-notifier' unless terminal_notifier_exist?
     time = args.shift.strip.to_i
     sleep time
     `terminal-notifier -message #{"Time is out dude !!!".dump} -title "TimeBlock - #{time}s" -sound "default"`
   rescue => e
     puts e.message
     exit(1)
+  end
+
+  def self.terminal_notifier_exist?
+    `which terminal-notifier`
+    $? == 0
   end
 end
